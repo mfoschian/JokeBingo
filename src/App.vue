b3963e71532ba250035256e7c7c0d071ba4d1093
Marco Foschian <marco.foschian@gmail.com>
2026-07-28 13:05:34 +0200

    css fixes and instructions

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ const forced_name = new URLSearchParams(location.search)?.get('d');
 const DEFAULT_DATASET = 'default';
 const DEFAULT_CARD = {
 	title: null,
+	instructions: null,
 	sentences: [],
 	guessed: []
 };
@@ -95,6 +96,7 @@ async function loadData() {
 		card.value.sentences = randomChoose(dataset.sentences, dataset?.directives?.itemstowin ?? DEFAULT_CARD_LENGTH);
 		card.value.guessed = [];
 		card.value.title = dataset?.directives?.title ?? null;
+		card.value.instructions = dataset?.directives?.instructions ?? null;
 	}
 	else {
 		card.value = c;
@@ -138,7 +140,7 @@ async function writeCardData(name, data) {
 }
 
 async function changeCard() {
-	const yesno = confirm("Cambio la cartella (perderai le frasi indovinate...)?");
+	const yesno = confirm("Se cambi cartella perderai le frasi indovinate. Sicuro ?");
 	if(yesno == true)
 		await clearCardData();
 }
@@ -169,38 +171,193 @@ onMounted( loadData );
 </script>
 
 <template>
-	<h1 class="title">{{ card.title ?? 'Joke' }} Bingo!</h1>
+	<h1 >Joke Bingo!</h1>
 	<template v-if="status == STATUS.ERROR">
 		<div class="error">
 			<h2>Mi spiace, si è verificato un errore:</h2>
 			<pre>
 				{{ error }}
 			</pre>
-			<hr>
+		</div>
+		<div class="buttons">
 			<button @click="clearCardData()">Resetta e riprova</button>
 		</div>
 	</template>
-	<template v-if="status == STATUS.LOADING">
+	<template v-else-if="status == STATUS.LOADING">
 		<h2>Caricamento in corso</h2>
 	</template>
-	<template v-if="status == STATUS.WON">
-		<h2>HAI VINTO !!!!</h2>
+	<template v-else-if="status == STATUS.WON">
+		<div class="title" v-if="card.title">{{ card.title }}</div>
+
+		<div class="won">HAI VINTO !!!!</div>
 		<div class="sentences">
 			<div class="sentence" v-for="(s,ix) in card.sentences">
 				<input type="checkbox" disabled :checked="card.guessed[ix]" @input="setGuess(ix,$event.target.checked)">{{ s }}</input>
 			</div>
 		</div>
-		<button @click="clearCardData()">Riprova con un'altra cartella</button>
+		<div class="buttons">
+			<button @click="clearCardData()">Gioca ancora con un'altra cartella</button>
+		</div>
 	</template>
 	<template v-if="status == STATUS.PLAYING">
-		<button @click="changeCard()">Abbandona cartella</button>
-		<hr>
+		<div class="instructions" v-if="card.instructions">{{ card.instructions }}</div>
+		<div class="title" v-if="card.title">{{ card.title }}</div>
+
 		<div class="sentences">
-			<div class="sentence" v-for="(s,ix) in card.sentences">
-				<input type="checkbox" :checked="card.guessed[ix]" @input="setGuess(ix,$event.target.checked)">{{ s }}</input>
+			<div class="sentence" v-for="(s,ix) in card.sentences" @click="setGuess(ix, ! (card.guessed[ix] ?? false))">
+				<div class="checkbox">
+					<input type="checkbox" :checked="card.guessed[ix]">{{ s }}</input>
+				</div>
+				<div class="thumb"><span>&#x1F44D;</span></div>
 			</div>
+		</div>
+		<div class="buttons">
+			<button @click="changeCard()">Cambia cartella</button>
 		</div>
 	</template>
 </template>
 
-<style scoped></style>
+<style>
+#app {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 1rem;
+
+	& > * {
+		max-width: 90%;
+	}
+}
+body {
+	background-color: #e7e7e7;
+}
+</style>
+
+<style scoped lang="scss">
+
+.title {
+	font-weight: bold;
+	font-size: 1.3rem;
+	text-align: center;
+
+	&:before,&:after {
+		content: "\"";
+	}
+}
+
+.won {
+	z-index: 1000;
+	padding: 3rem;
+	font-size: 2rem;
+	font-weight: bold;
+	background-color: rgb(253, 64, 64);
+	border-radius: 1rem;
+	color: #ffe409;
+	position: absolute;
+	top: 30%;
+	width: 45%;
+	text-align: center;
+	
+
+	@keyframes tilt-n-move-shaking {
+	  0% { transform: translate(0, 0) rotate(0deg); }
+	  25% { transform: translate(5px, 5px) rotate(5deg); }
+	  50% { transform: translate(0, 0) rotate(0eg); }
+	  75% { transform: translate(-5px, 5px) rotate(-5deg); }
+	  100% { transform: translate(0, 0) rotate(0deg); }
+	}
+
+	animation: tilt-n-move-shaking .3s infinite;
+}
+
+.instructions {
+	border: 1px dashed black;
+	border-radius: 1rem;
+	padding: .8rem;
+
+	font-style: italic;
+	font-size: 1.1rem;
+	text-align: center;
+}
+
+.sentences {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	.sentence {
+		--sentence-color: rgb(130, 253, 253);
+		--sentence-color: white;
+		--sentence-ok-color: #53e753;
+
+		// justify-self: stretch;
+		padding: 1rem;
+		background-color: var(--sentence-color);
+		border: 1px transparent contrast-color(var(--sentence-color));
+		border-radius: .5rem;
+		font-weight: bold;
+		
+		transition: background-color .5s ease-out;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 1rem;
+		// justify-items: center;
+		// justify-content: center;
+
+		input[type="checkbox"] {
+			appearance: none;
+		}		
+
+		overflow: hidden;
+		.thumb {
+			// display:none;
+			flex-basis: 0;
+			transform: rotate(-180deg);
+			transition: transform .3s ease-in-out;
+			
+			span {
+				padding-left:.5rem;
+				padding-right:.5rem;
+				opacity: 0;
+
+				transition: opacity .3s ease-in-out;
+			}
+		}
+
+		.checkbox {
+			flex-basis: 1;
+			flex-grow: 1;
+			text-align: center;
+		}
+
+		&:has(input[type="checkbox"]:checked) {
+			--sentence-color: var(--sentence-ok-color);
+
+			.thumb {
+				transform: rotate(0deg);
+
+				span {
+					opacity: 1;
+				}
+			}
+		}		
+
+	}
+}
+
+
+.buttons {
+	margin-top: 1rem;
+
+	button {
+		--btn-color: beige;
+		padding: .7rem;
+		background-color: var(--btn-color);
+		border: 1px solid contrast-color(var(--btn-color));
+		border-radius: .7rem;
+
+		box-shadow: 5px 5px 5px rgb(52, 51, 20);
+	}
+}
+</style>
